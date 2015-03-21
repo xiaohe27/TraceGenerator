d045e5878940ccaa5ae30ececd7553a65ead6f5e
xiaohe27 <xiaoguoyi27@gmail.com>
2015-03-21 11:23:49 -0500

    clean

--- src/BenchmarkFromYi/Bench2/program/src/LockTest.java
+++ src/BenchmarkFromYi/Bench2/program/src/LockTest.java
@@ -5,7 +5,7 @@ public class LockTest {
 
   public static int sum = 0;
 
-    class MyObj {
+    static class MyObj {
         Lock lock;
 
         public MyObj(Lock lock) {
@@ -19,130 +19,27 @@ public class LockTest {
             this.lock.unlock();
             return this.lock;
         }
+
+        public void f(int num) {
+            for (int i = 0; i < num; i++) {
+                this.acquire();
+            }
+
+            for (int i = 0; i < num; i++) {
+                this.release();
+            }
+
+        }
     }
 
   public static void main(String[] args) {
- 
-    int depth = Integer.parseInt(args[0]);
+    MyObj myObj1 = new MyObj(new ReentrantLock());
 
-    Lock l1 = new ReentrantLock();
-    Lock l2 = new ReentrantLock();
-    Lock l3 = new ReentrantLock();
-
-    Thread t1 = new CThread(l1, "thread1", 1, depth);
-    t1.start();
-    Thread t2 = new CThread(l1, "thread2", 2, depth);
-    t2.start();
-    Thread t3 = new CThread(l1, "thread3", 3, depth);
-    t3.start();
-    Thread t4 = new WThread1(l2, "thread4", 4, depth);
-    t4.start();
-    Thread t5 = new CThread(l2, "thread5", 5, depth);
-    t5.start();
-    Thread t6 = new CThread(l2, "thread6", 6, depth);
-    t6.start(); 
-
-    try {
-      t1.join();
-      t2.join();
-      t3.join();
-      t4.join();
-      t5.join();
-      t6.join();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-   System.out.println("The result is " + sum);
+      myObj1.f(10);
     
   }
 }
 
-
-class CThread extends Thread {//good thread
-
-  private Lock lo;
-  private int tNum;
-  private int depth;
-
-  public CThread(Lock l, String name, int threadNo, int dep) {
-    this.lo = l;
-    this.setName(name);
-    this.tNum = threadNo;
-    this.depth = dep;
-  }
-
-  public void doSomething() { //balanced
-    if (depth == 1) {
-      try {
-        lo.lock();
-        LockTest.sum += this.tNum;
-        Thread.sleep(1000);
-      } catch (Exception e) {
-        System.err.println(e);
-      } finally {
-        lo.unlock();
-      }
-    } else {
-      try{
-        depth--;
-        lo.lock();
-        doSomething();
-      } catch (Exception e){
-        System.err.println(e);
-      } finally {
-        lo.unlock();
-      }
-    }
-   
-  }
-
-  public void run() {
-    doSomething();
-  }
-}
-
-
-class WThread1 extends Thread {
-  private Lock lo;
-  private int tNum;
-  private int depth;
-
-  public WThread1(Lock l, String name, int threadNo, int dep) {
-    this.lo = l;
-    this.setName(name);
-    this.tNum = threadNo;
-    this.depth = dep;
-  }
-
-  public void doSomething() {
-    if (depth == 1) {
-      try {
-        lo.lock();
-        LockTest.sum += this.tNum;
-	System.out.println("Oops,thread "+ this.tNum + " may cause deadlock!");
-        Thread.sleep(1000);
-      } catch (Exception e) {
-        System.err.println(e);
-      } 
-    } else {
-      try{
-        depth--;
-        lo.lock();
-        doSomething();
-      } catch (Exception e){
-        System.err.println(e);
-      } finally {
-        lo.unlock();
-      }
-    }
-   
-  }
-
-  public void run() {
-    doSomething();
-  }
-}
 
 
   
